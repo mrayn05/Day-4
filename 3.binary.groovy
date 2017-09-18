35cab5a9a1624dc2831234147445bfa11e48d109
Max Ryan <maxryan12345@hotmail.com>
2017-09-19 00:00:20 +0100

    First method done

---
+++ 3.binary.groovy
@@ -0,0 +1,13 @@
+int power (int base, int exponent) {
+	System.out.println("Enter a base number: ");
+	int base = Integer.parseInt(System.console().readLine());
+	System.out.println("Enter an exponent: ");
+	int exponent = Integer.parseInt(System.console().readLine());
+	int num = 0;
+	
+	for (int i = 1; i < exponent; i++); {
+		num = base * exponent;
+	}
+	System.out.println(num);
+	return num;
+}
